af197d878615a98586659c383953ff08dba0e58f
Maycon Kevin <mayconkevinribeiro@hotmail.com>
2020-11-05 23:43:43 -0300

    Projeto criado

---
+++ src/main/java/br/com/mayconkevin/excaliburspring/ExcaliburSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.mayconkevin.excaliburspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcaliburSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcaliburSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mayconkevin/excaliburspring/ExcaliburSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mayconkevin.excaliburspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcaliburSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
